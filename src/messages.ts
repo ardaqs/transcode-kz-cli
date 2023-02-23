443ecd70b5fd532eb6ded8c1d654c75e7268c9ba
Ardaq Saduaqas <ardaqs@gmail.com>
2023-02-23 17:55:15 +0600

    Messages

---
+++ src/messages.ts
@@ -0,0 +1,38 @@
+export const man = `\u001b[1mNAME\u001b[0m
+    transcode-kz-cli - transcode Kazakh between Cyrillic and Latin encodings
+
+\u001b[1mSYNOPSIS\u001b[0m
+    \u001b[1mtranscode-kz-cli\u001b[0m [\u001b[4mOPTION\u001b[0m]... \u001b[4mSOURCE\u001b[0m [\u001b[4mDEST\u001b[0m]
+    \u001b[1mtranscode-kz-cli\u001b[0m --help
+
+\u001b[1mDESCRIPTION\u001b[0m
+    Transcode Kazakh between Cyrillic and Latin encodings.
+
+\u001b[1mOPTIONS\u001b[0m
+    \u001b[1m-d\u001b[0m=\u001b[4mDEVICE\u001b[0m, \u001b[1m--device\u001b[0m=\u001b[4mDEVICE\u001b[0m
+        \u001b[1mconsole\u001b[0m \u001b[2m(default)\u001b[0m  - Use console for input/output
+        \u001b[1mfile\u001b[0m               - Use file(s) for input/output
+
+    \u001b[1m-f\u001b[0m=\u001b[4mFORMAT\u001b[0m, \u001b[1m--format\u001b[0m=\u001b[4mFORMAT\u001b[0m
+        \u001b[1mtext\u001b[0m \u001b[2m(default)\u001b[0m  - transcode as plain text
+        \u001b[1mjson\u001b[0m            - transcode only string values in the JSON
+
+    \u001b[1m-h\u001b[0m, \u001b[1m--help\u001b[0m
+        Display this help and exit
+
+\u001b[1mEXAMPLES\u001b[0m
+    \u001b[1mtranscode-kz-cli\u001b[0m "Қазақша мәтін"
+    \u001b[1mtranscode-kz-cli\u001b[0m -f=json "{ \"country\": \"Қазақстан\" }"
+    \u001b[1mtranscode-kz-cli\u001b[0m -d=file -f=text doc-cyr.txt doc-lat.txt
+    \u001b[1mtranscode-kz-cli\u001b[0m -d=file -f=json src/assets/i18n/kk-Cyrl.json src/assets/i18n/kk-Latn.json
+    \u001b[1mtranscode-kz-cli\u001b[0m -h
+`;
+
+export const info = `Transcoding...`;
+
+export const success = `\u001b[32mSuccess\u001b[0m`;
+
+export const errUsage = `\u001b[33mUsage:\u001b[0m \u001b[1mtranscode-kz-cli\u001b[0m [\u001b[4mOPTION\u001b[0m]... \u001b[4mSOURCE\u001b[0m [\u001b[4mDEST\u001b[0m]
+Try 'transcode-kz-cli --help' for more information.`;
+
+export const errWrongArgs = `\u001b[31mError:\u001b[0m Wrong argument(s)`;
